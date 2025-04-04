c1c324fad8f9128c77c57224e399865f84e4b918
Ivan Zyranau <ewancoder@gmail.com>
2025-04-04 23:16:26 +0400

    Add NTFS-3G driver for NTFS mounting

--- install.sh
+++ install.sh
@@ -54,6 +54,7 @@ special_packages=(
     unzip       # Archives management. For now I only needed to unzip. Will add others here later.
 
     btrfs-progs # Utility to manage BTRFS filesystem. Might be handy to manipulate drives live.
+    ntfs-3g     # NTFS driver.
 )
 
 # Essential packages for a working WM environment.
