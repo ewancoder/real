be8e35a8de3894e9425d82ebfec29684a53d8316
Ivan Zyranau <ewancoder@gmail.com>
2025-10-11 03:05:41 +0400

    Add an example packages profile

---
+++ packages/example.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+# Example of a packages profile.
+
+# Installed during system install (pacman).
+packages+=(
+    flatpak
+    freerdp
+)
+
+# Installed after reboot and first boot up.
+firstboot_packages+=(
+    nvidia-open
+    lib32-nvidia-utils
+)
+
+# Enabled as systemd service.
+services+=(
+    docker
+    sshd
+)
+
+# Installed using flatpak globally in the system.
+flatpak+=(
+    us.zoom.Zoom
+    com.slack.Slack
+)
